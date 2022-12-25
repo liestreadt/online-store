e6c23c302e7923e28c6c8a6ca59f9ca5c8c68e98
neemkashu <sparjinkust@mail.ru>
2022-12-25 18:36:45 +0300

    feat: add handler templates for events

--- src/app/Controller.ts
+++ src/app/Controller.ts
@@ -1,7 +1,7 @@
 import View from './View';
 import Model from './Model';
 
-interface ButtonsStore {
+interface ElementsToListenStore {
     reset?: HTMLButtonElement;
     copy?: HTMLButtonElement;
     category: HTMLDivElement;
@@ -30,31 +30,60 @@ class Controller {
     constructor () {
         this.model = new Model(document.location.href);
         this.view = new View(this.model.modelData);
+        this.addListeners();
+    }
+    initViewAndListeners() {
+        this.view = new View(this.model.modelData);
+        this.addListeners();
     }
     addListeners() {
         switch (this.model.modelData.page) {
             case 'store': {
-                const buttons: Partial<ButtonsStore> = {} // = this.view.getButtonsArray();
+                const elementsToListen: Partial<ElementsToListenStore> = {} // = this.view.getElementsForEvents();
 
                 console.log('add event listeners to buttons and other inputs');
                 console.log('be sure handle events by class handler');
-                buttons.reset?.addEventListener('click', this);
+                elementsToListen.reset?.addEventListener('click', this);
             }
         }
     }
     handleEvent(event: Event): void {
         if (event.target instanceof HTMLElement) {
+
             switch (event.target.id) {
+            // this switch case is not pretty;
+            // any suggestions how to construct method name from
+            // event.target and call it are welcome!
+
                 case eventTargetsID.reset:{
+                    this.resetEvent();
                     break;
                 }
                 case eventTargetsID.copy:{
+                    this.copyEvent();
                     break;
                 }
                 case eventTargetsID.category:{
+                    this.copyEvent();
                     break;
                 }
                 case eventTargetsID.brand:{
+                    this.categoryEvent(event);
+                    break;
+                }
+                case eventTargetsID.price:{
+                    break;
+                }
+                case eventTargetsID.stock:{
+                    break;
+                }
+                case eventTargetsID.sorting:{
+                    break;
+                }
+                case eventTargetsID.searching:{
+                    break;
+                }
+                case eventTargetsID.viewButtons:{
                     break;
                 }
             }
@@ -67,6 +96,37 @@ class Controller {
         }
     }
     private resetEvent() {
+        console.log('this.model.resetFilters()');
+        this.initViewAndListeners();
+    }
+    private copyEvent() {
+        console.log('this.view.copyURLtoClipboard()');
+    }
+    private categoryEvent(event: Event) {
+        console.log('be prepared to handle both input or label click');
+        if (event.currentTarget instanceof HTMLElement) {
+            console.log(`
+                this.model.applyFilter(event.currentTarget.id);
+            `);
+            this.initViewAndListeners();
+        }
+    }
+    private brandEvent() {
+
+    }
+    private priceEvent() {
+
+    }
+    private stockEvent() {
+
+    }
+    private sortingEvent() {
+
+    }
+    private searchingEvent() {
+
+    }
+    private viewButtonsEvent() {
 
     }
 }
