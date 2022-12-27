01ef504d2c28c154619ef6554101b3ab8f20f092
neemkashu <sparjinkust@mail.ru>
2022-12-27 11:18:25 +0300

    feat: add basic handleEvent method and listeners

--- src/dual-slider/dual.ts
+++ src/dual-slider/dual.ts
@@ -1,40 +1,21 @@
-const template = `
-<div class="dual-slider__indicators">
-    <div id="dual-slider-label-min" class="dual-slider__label">0</div>
-    <div for="dual-slider-label-max" class="dual-slider__label">100</div>
-</div>
-<div class="dual-slider__inputs">
-    <input
-        type="range"
-        id="dual-slider-min"
-        class="dual-slider__input"
-        min="0"
-        max="1000"
-        >
-    <input
-        type="range"
-        id="dual-slider-max"
-        class="dual-slider__input dual-slider__max"
-        min="0"
-        max="1000"
-        >
-</div>
-`;
 export class DualSlider {
     protected _minScale: number;
     protected _maxScale: number;
     protected _inputMin: number;
     protected _inputMax: number;
+    protected container: HTMLElement;
+
     constructor(min: number, max: number) {
         this._minScale = min;
         this._maxScale = max;
         this._inputMin = min;
         this._inputMax = max;
+        this.container = document.createElement('div');
+        this.container.className = 'dual-slider';
     }
-    private createSlider() {
-        const container = document.createElement('div');
-        container.className = 'dual-slider';
-        container.innerHTML = `
+
+    private updateSlider() {
+        this.container.innerHTML = `
 <div class="dual-slider__indicators">
     <div id="dual-slider-label-min" class="dual-slider__label">${this.inputMin}</div>
     <div for="dual-slider-label-max" class="dual-slider__label">${this.inputMax}</div>
@@ -44,7 +25,7 @@ export class DualSlider {
         type="range"
         id="dual-slider-min"
         class="dual-slider__input"
-        min="${this.minScale}"
+        min="${this._minScale}"
         max="${this.maxScale}"
         value="${this.inputMin}"
         >
@@ -52,25 +33,43 @@ export class DualSlider {
         type="range"
         id="dual-slider-max"
         class="dual-slider__input dual-slider__max"
-        min="${this.minScale}"
+        min="${this._minScale}"
         max="${this.maxScale}"
         value="${this.inputMax}"
         >
 </div>
         `;
-        return container;
+        this.addListeners();
+        return this.container;
+    }
+    private addListeners() {
+        const elementMax = this.container.querySelector('#dual-slider-max');
+        elementMax?.addEventListener('input', this);
+        const elementMin = this.container.querySelector('#dual-slider-min');
+        elementMin?.addEventListener('input', this);
     }
     insertSlider(parent: HTMLElement) {
-        parent.appendChild(this.createSlider());
+        parent.appendChild(this.updateSlider());
+        this.addListeners();
+    }
+    handleEvent(event: Event) {
+        if (event.currentTarget instanceof HTMLInputElement) {
+            if (event.currentTarget.id === 'dual-slider-max') {
+                this.inputMax = +event.currentTarget.value;
+            }
+            console.log('value', event.currentTarget.value);
+        }
     }
     set minScale(newMin: number) {
         this._minScale = newMin;
+        this.updateSlider();
     }
     get minScale(): number {
         return this._minScale;
     }
     set maxScale(newMax: number) {
         this._maxScale = newMax;
+        this.updateSlider();
     }
     get maxScale(): number {
         return this._maxScale;
@@ -88,3 +87,9 @@ export class DualSlider {
         return this._inputMax;
     }
 }
+//import { DualSlider } from './dual-slider/dual';
+// const dualSlider = new DualSlider(0, 500);
+// const main = document.querySelector('main');
+// if (main !== null) {
+//     dualSlider.insertSlider(main);
+// }
