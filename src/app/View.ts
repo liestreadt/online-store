6a6213b52537fbed29e0e4a29e2a9ef3ec858280
Vlad_nikishin <Nik_vladislav@outlook.com>
2022-12-29 01:04:02 +0300

    refactor: create ts modules that create page containers

--- src/app/View.ts
+++ src/app/View.ts
@@ -1,4 +1,9 @@
 import { DualSlider } from '../dual-slider/dual';
+import createSortingSection from './view-methods/createSortingSection';
+import createModal from './view-methods/createModal';
+import createFooter from './view-methods/createFooter';
+import createHeader from './view-methods/createHeader';
+import createStoreFilters from './view-methods/createStoreFilters';
 
 import {
     ProductDetail,
@@ -51,8 +56,8 @@ class View {
         if (containerMain) {
             containerMain.outerHTML = `
                 <main class="main-store">
-                    ${this.getStoreFilters()}
-                    ${this.getSortingSection()}
+                    ${this.getStoreFilters(this.modelData)}
+                    ${this.getSortingSection(this.modelData)}
                 </main>
             `;
         }
@@ -78,161 +83,24 @@ class View {
             `;
         }
     }
-    renderHeader() {
-        document.body.innerHTML = `
-            <header class="header">
-                <a href="#!" class="header__logo">
-                    <span class="header__logo-bag">
-                        🛍
-                    </span>
-                    <h1 class="header__store-name">Online Store</h1>
-                </a>
-                <p class="header__total">
-                    Cart total: <span class="header__total-price">€6,232.00</span>
-                </p>
-                <a href="#!" class="header__goods-number">10</a>
-            </header>
-        `;
-    }
     renderMain() {
-        document.body.innerHTML += `
-            <main></main>
-        `;
+        document.body.innerHTML += `<main></main>`;
+    }
+    renderHeader() {
+        document.body.innerHTML = createHeader();
     }
     renderFooter() {
-        document.body.innerHTML += `
-            <footer class="footer">
-                <p class="footer__year">2022</p>
-                <a href="https://github.com/neemkashu" class="footer__github">neemkashu</a>
-                <a href="https://github.com/liestreadt" class="footer__github">liestreadt</a>
-                <a href="https://rs.school/js/" class="footer__logo"></a>
-            </footer>
-        `;
+        document.body.innerHTML += createFooter();
     }
     renderModal() {
-        document.body.innerHTML += `
-            <section class="modal">
-                <form class="modal__window">
-                    <h2 class="modal__header">Personal details</h2>
-                    <div class="modal__name">
-                        <input
-                            type="text"
-                            placeholder="Name"
-                            class="modal__name-input">
-                    </div>
-                    <div class="modal__number">
-                        <input
-                            type="text"
-                            placeholder="Phone number"
-                            class="modal__number-input">
-                    </div>
-                    <div class="modal__address">
-                        <input
-                            type="text"
-                            placeholder="Delivery address"
-                            class="modal__address-input">
-                    </div>
-                    <div class="modal__e-mail">
-                        <input
-                            type="email"
-                            placeholder="E-mail"
-                            class="modal__e-mail-input">
-                    </div>
-                    <div class="modal__debit-card">
-                        <h2 class="modal__header">Debit / Credit card details</h2>
-                        <div class="modal__debit-card-data">
-                            <div class="modal__debit-card-number">
-                                <input
-                                    type="text"
-                                    placeholder="Enter card number"
-                                    class="modal__debit-card-number-input">
-                            </div>
-                            <div class="modal__debit-card-other">
-                                Valid to: <input class="modal__debit-card-valid-to-input">
-                                CVC2 / CVV2: <input class="modal__debit-card-cvc-input">
-                            </div>
-                        </div>
-                        <button class="modal__btn-confirm">
-                            CONFIRM
-                        </button>
-                    </div>
-                </form>
-            </section>
-        `;
+        document.body.innerHTML += createModal();
     }
-    getStoreFilters() {
-        return `
-            <section class="side-filter">
-                <div class="side-filter__control-buttons">
-                    <button id="${EventTargetsIDEnum.reset}" class="side-filter__reset">
-                        Reset Filters
-                    </button>
-                    <button id="${EventTargetsIDEnum.copy}" class="side-filter__copy">
-                        Copy Link
-                    </button>
-                </div>
-                <div class="side-filter__field">
-                    <h2 class="side-filter__filter-header header_small">
-                        Categories
-                    </h2>
-                    <div id="${EventTargetsIDEnum.category}" class="side-filter__items-container">
-                        ${this.modelData.allCategories
-                            ?.map((i) => {
-                                return `
-                                    <div class="category-item">
-                                        <input
-                                            type="checkbox"
-                                            id="input-${i}"
-                                            class="filter-item__checkbox">
-                                        <label
-                                            for="input-${i}"
-                                            class="filter-item__label">
-                                            ${i}
-                                        </label>
-                                        <span class="filter-item__amount">(5/5)</span>
-                                    </div>
-                                `;
-                            })
-                            .join('')}
-                    </div>
-                </div>
-                <div class="side-filter__field">
-                    <h2 class="side-filter__filter-header header_small">
-                        Brands
-                    </h2>
-                    <div id="${EventTargetsIDEnum.brand}" class="side-filter__items-container">
-                        ${this.modelData.allBrands
-                            ?.map((i) => {
-                                return `
-                                    <div class="brand-item">
-                                        <input
-                                            type="checkbox"
-                                            id="input-${i}"
-                                            class="filter-item__checkbox">
-                                        <label
-                                            for="input-${i}"
-                                            class="filter-item__label">
-                                            ${i}
-                                        </label>
-                                        <span class="filter-item__amount">(5/5)</span>
-                                    </div>
-                                `;
-                            })
-                            .join('')}
-                    </div>
-                </div>
-                <div id="${EventTargetsIDEnum.price}" class="side-filter__field">
-                    <h2 class="side-filter__filter-header header_small">
-                        Price
-                    </h2>
-                </div>
-                <div id="${EventTargetsIDEnum.stock}" class="side-filter__field">
-                    <h2 class="side-filter__filter-header header_small">
-                        Stock
-                    </h2>
-                </div>
-            </section>
-        `;
+
+    getStoreFilters(data: Partial<ModelData>): string {
+        return createStoreFilters(data);
+    }
+    getSortingSection(data: Partial<ModelData>) {
+        return createSortingSection(data);
     }
     getDualSlider() {
         const sliderContainerPrice = document.querySelector(`#${EventTargetsIDEnum.price}`) as HTMLElement;
@@ -241,86 +109,6 @@ class View {
         const dualSliderStock = new DualSlider(0, 500);
         if (sliderContainerPrice) dualSliderPrice.insertSlider(sliderContainerPrice);
         if (sliderContainerStock) dualSliderStock.insertSlider(sliderContainerStock);
-    }
-    getSortingSection() {
-        return `
-            <section class="sorting">
-                <div class="sorting-header">
-                    <select name="" id="${EventTargetsIDEnum.sorting}" class="sorting__menu">
-                        <option value="default-sort" class="sorting-option" selected>Sort default</option>
-                        <option value="ascending-price" class="sorting-option">Sort by price (ascending)</option>
-                        <option value="desending-price" class="sorting-option">Sort by price (descending)</option>
-                        <option value="ascending-rating" class="sorting-option">Sort by rating (ascending)</option>
-                        <option value="descending-rating" class="sorting-option">Sort by rating (descending)</option>
-                    </select>
-                    <div class="sorting__total-found">
-                        Found: <span class="sorting__amount">${this.modelData.filteredProducts?.length}</span>
-                    </div>
-                    <input
-                        type="text"
-                        name=""
-                        id="${EventTargetsIDEnum.searching}"
-                        class="sorting__search"
-                        placeholder="Search product">
-                    <div id="${EventTargetsIDEnum.viewButtons}" class="sorting__view-buttons">
-                        Choose view: <button class="sorting__tiny-view">📱</button>
-                        <button class="sorting__small-view">💻</button>
-                    </div>
-                </div>
-                <div class="sorting__card-container sorting__card-container_small">
-                    ${this.modelData.filteredProducts?.map((i) => this.getCard(i)).join('')}
-                </div>
-            </section>
-        `;
-    }
-    getCard(obj: ProductDetail) {
-        return `
-            <div class="product-card">
-                <h2 class="product-card__header header_small">
-                    ${obj.title}
-                </h2>
-                <div
-                    class="product-card__body"
-                    style="
-                        background: url('${
-                            obj.images.filter((i) => i.match(/thumbnail/)).length
-                                ? obj.images.filter((i) => i.match(/thumbnail/))
-                                : obj.images[0]
-                        }');
-                        background-repeat: no-repeat;
-                        background-position: center;
-                        background-size: cover;">
-                    <div class="product-card__info">
-                        <div class="product-card__info-item">
-                            Category: <span class="product-card__category-text">${obj.category}</span>
-                        </div>
-                        <div class="product-card__info-item">
-                            Brand: <span class="product-card__brand-text">${obj.brand}</span>
-                        </div>
-                        <div class="product-card__info-item">
-                            Price: <span class="product-card__price-text">€${obj.price}</span>
-                        </div>
-                        <div class="product-card__info-item">
-                            Discount: <span class="product-card__discount-text">${obj.discountPercentage} %</span>
-                        </div>
-                        <div class="product-card__info-item">
-                            Rating: <span class="product-card__rating-text">${obj.rating}</span>
-                        </div>
-                        <div class="product-card__info-item">
-                            Stock: <span class="product-card__stock-text">${obj.stock}</span>
-                        </div>
-                    </div>
-                    <div class="product-card__controls">
-                        <button class="product-card__btn-add-to-cart">
-                            ADD TO CART
-                        </button>
-                        <a href="#!" class="product-card__link-to-details">
-                            DETAILS
-                        </a>
-                    </div>
-                </div>
-            </div>
-        `;
     }
     getProdDetailsContainer(
         obj: ProductDetail = {
---
+++ src/app/view-methods/createModal.ts
@@ -0,0 +1,51 @@
+export default function createModal(): string {
+    return `
+        <section class="modal">
+            <form class="modal__window">
+                <h2 class="modal__header">Personal details</h2>
+                <div class="modal__name">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        class="modal__name-input">
+                </div>
+                <div class="modal__number">
+                    <input
+                        type="text"
+                        placeholder="Phone number"
+                        class="modal__number-input">
+                </div>
+                <div class="modal__address">
+                    <input
+                        type="text"
+                        placeholder="Delivery address"
+                        class="modal__address-input">
+                </div>
+                <div class="modal__e-mail">
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        class="modal__e-mail-input">
+                </div>
+                <div class="modal__debit-card">
+                    <h2 class="modal__header">Debit / Credit card details</h2>
+                    <div class="modal__debit-card-data">
+                        <div class="modal__debit-card-number">
+                            <input
+                                type="text"
+                                placeholder="Enter card number"
+                                class="modal__debit-card-number-input">
+                        </div>
+                        <div class="modal__debit-card-other">
+                            Valid to: <input class="modal__debit-card-valid-to-input">
+                            CVC2 / CVV2: <input class="modal__debit-card-cvc-input">
+                        </div>
+                    </div>
+                    <button class="modal__btn-confirm">
+                        CONFIRM
+                    </button>
+                </div>
+            </form>
+        </section>
+    `;
+}
