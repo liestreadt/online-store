7d2fcfd8cbd57659cceacf6fdb06757d09470dfc
neemkashu <sparjinkust@mail.ru>
2023-01-03 12:42:06 +0300

    refactor: add array of search fields in checkProductPassSearching function

--- src/app/FilterCalculator.ts
+++ src/app/FilterCalculator.ts
@@ -1,4 +1,4 @@
-import { ProductDetail, ShownProductInfo } from './intefaces/types';
+import { FilterKeys, ProductDetail, ShownProductInfo } from './intefaces/types';
 import increaseValueInMap from './tools/Functions';
 
 export class FilterCalculator {
@@ -46,25 +46,22 @@ export class FilterCalculator {
     updateSearchName(newSearchName: string) {
         this.searchName = newSearchName.toLowerCase();
     }
-    isProductPassSearching(product: ProductDetail): boolean {
-        const isTextInName = product.title.toLowerCase().includes(this.searchName);
-        const isTextInDescription = product.description.toLowerCase().includes(this.searchName);
-        const isTextInPrice = product.price.toString().toLowerCase().includes(this.searchName);
-        const isTextInDiscount = product.discountPercentage.toString().toLowerCase().includes(this.searchName);
-        const isTextInBrand = product.brand.toLowerCase().includes(this.searchName);
-        const isTextInCategory = product.category.toLowerCase().includes(this.searchName);
-        const isTextInRating = product.rating.toString().toLowerCase().includes(this.searchName);
-        const isTextInStock = product.stock.toString().toLowerCase().includes(this.searchName);
-        return (
-            isTextInName ||
-            isTextInDescription ||
-            isTextInPrice ||
-            isTextInDiscount ||
-            isTextInBrand ||
-            isTextInCategory ||
-            isTextInRating ||
-            isTextInStock
+    checkProductPassSearching(product: ProductDetail): boolean {
+        type productKey = keyof typeof product;
+        const fieldsToSearch: productKey[] = [
+            'title',
+            'description',
+            'price',
+            'discountPercentage',
+            'brand',
+            'category',
+            'rating',
+            'stock',
+        ];
+        const isProductPassSearching = fieldsToSearch.some((key) =>
+            product[key].toString().toLowerCase().includes(this.searchName)
         );
+        return isProductPassSearching;
     }
     checkProductPassFilters(product: ProductDetail): boolean {
         if (this.categories.size > 0 && !this.categories.has(product.category)) {
@@ -79,7 +76,7 @@ export class FilterCalculator {
         if (this.minUserStock > product.stock || this.maxUserStock < product.stock) {
             return false;
         }
-        if (!this.isProductPassSearching(product)) {
+        if (!this.checkProductPassSearching(product)) {
             return false;
         }
         return true;
