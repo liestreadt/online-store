e55d020bf6611135ecb26e2a79b6418a0d6eafc8
neemkashu <sparjinkust@mail.ru>
2023-01-03 11:53:17 +0300

    fix: add searching in all product info fields

--- src/app/FilterCalculator.ts
+++ src/app/FilterCalculator.ts
@@ -46,6 +46,26 @@ export class FilterCalculator {
     updateSearchName(newSearchName: string) {
         this.searchName = newSearchName.toLowerCase();
     }
+    isProductPassSearching(product: ProductDetail): boolean {
+        const isTextInName = product.title.toLowerCase().includes(this.searchName);
+        const isTextInDescription = product.description.toLowerCase().includes(this.searchName);
+        const isTextInPrice = product.price.toString().toLowerCase().includes(this.searchName);
+        const isTextInDiscount = product.discountPercentage.toString().toLowerCase().includes(this.searchName);
+        const isTextInBrand = product.brand.toLowerCase().includes(this.searchName);
+        const isTextInCategory = product.category.toLowerCase().includes(this.searchName);
+        const isTextInRating = product.rating.toString().toLowerCase().includes(this.searchName);
+        const isTextInStock = product.stock.toString().toLowerCase().includes(this.searchName);
+        return (
+            isTextInName ||
+            isTextInDescription ||
+            isTextInPrice ||
+            isTextInDiscount ||
+            isTextInBrand ||
+            isTextInCategory ||
+            isTextInRating ||
+            isTextInStock
+        );
+    }
     checkProductPassFilters(product: ProductDetail): boolean {
         if (this.categories.size > 0 && !this.categories.has(product.category)) {
             return false;
@@ -59,7 +79,7 @@ export class FilterCalculator {
         if (this.minUserStock > product.stock || this.maxUserStock < product.stock) {
             return false;
         }
-        if (!product.title.toLowerCase().includes(this.searchName)) {
+        if (!this.isProductPassSearching(product)) {
             return false;
         }
         return true;
